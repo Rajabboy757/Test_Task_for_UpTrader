543a5f7e34cf904c957519da0459dada32b1d877
Rajabboy757 <rajrajrajradju@gmail.com>
2023-10-19 15:06:12 +0500

    created model for menu

--- trader/models.py
+++ trader/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=30)
+    is_main = models.BooleanField(default=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('menu', kwargs={'menu_id': self.pk})
